e7d2490a9a9805161a58d2b9fdbe90c569f96b33
S1mpledboy <48122967+S1mpledboy@users.noreply.github.com>
2022-07-26 00:13:25 +0300

    Initial commit

---
+++ reverse_snake_game.js
@@ -0,0 +1,15 @@
+import Snake from './snake.js'
+let snake = new Snake(3,1);
+function main_loop() {
+	console.log(snake.speed)
+	update();
+	render();
+}
+main_loop()
+function update(){
+
+}
+
+function render(){
+
+}
---
+++ snake.js
@@ -0,0 +1,7 @@
+export class Snake{
+	speed = 2;
+	constructor({weight_impact,difficulty}){
+		this.weight_impact = weight_impact;
+		this.difficulty = difficulty;
+	}
+}
